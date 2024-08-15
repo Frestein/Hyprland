fd03d91b3517a601e55e232764dab35fe68a109b
Frestein <Frestein@tuta.io>
2024-08-15 16:49:47 +0500

    fix(scripts/killactive): add notification dismissal on window close confirmation

--- scripts/killactive.sh
+++ scripts/killactive.sh
@@ -6,9 +6,10 @@ state_file="$dir/.kill_confirmed"
 
 if [ -f "$state_file" ]; then
   rm "$state_file"
+  makoctl dismiss -g
   hyprctl dispatch killactive
 else
   notify-send -u low -h string:x-dunst-stack-tag:killactive 'You want to close the window?'
   touch "$state_file"
-  (sleep 3 && rm -f "$state_file") &
+  (sleep 2 && rm -f "$state_file") &
 fi
