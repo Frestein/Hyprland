9781626ad43bc754ce5dce9ca7ebefabc1ca82d0
Frestein <Frestein@tuta.io>
2025-01-04 04:16:25 +0500

    refactor(zen-mode): clean up

--- scripts/zen-mode.sh
+++ scripts/zen-mode.sh
@@ -1,27 +1,21 @@
 #!/bin/dash
 
-state_file="$HOME/.zen_mode_state"
+HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
 
-if [ -f "$state_file" ]; then
-  hyprctl keyword general:gaps_in 5
-  hyprctl keyword general:gaps_out 12
-  hyprctl keyword decoration:rounding 0
-  hyprctl keyword animations:enabled true
-
-  pkill -SIGUSR1 waybar
-
-  makoctl mode -r do-not-disturb
-
-  rm "$state_file"
-else
-  hyprctl keyword general:gaps_in 0
-  hyprctl keyword general:gaps_out 0
-  hyprctl keyword decoration:rounding 0
-  hyprctl keyword animations:enabled false
-
-  pkill -SIGUSR1 waybar
-
-  makoctl mode -a do-not-disturb
-
-  touch "$state_file"
+if [ "$HYPRGAMEMODE" = 1 ]; then
+	pkill -SIGUSR1 waybar
+	hyprctl --batch "\
+        keyword animations:enabled 0;\
+        keyword decoration:shadow:enabled 0;\
+        keyword decoration:blur:enabled 0;\
+        keyword general:gaps_in 0;\
+        keyword general:gaps_out 0;\
+        keyword general:border_size 2;\
+        keyword decoration:rounding 0"
+	makoctl mode -a do-not-disturb
+	exit
 fi
+
+pkill -SIGUSR1 waybar
+hyprctl reload
+makoctl mode -r do-not-disturb
