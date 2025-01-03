82f5b24fb3e84c0efc8aa5e44e2606f7f2d95721
Frestein <Frestein@tuta.io>
2025-01-03 14:26:13 +0500

    feat: add brightness.sh

---
+++ scripts/brightness.sh
@@ -0,0 +1,30 @@
+#!/bin/dash
+
+notify_cmd='notify-send -u low -h string:x-dunst-stack-tag:brightness'
+
+get_brightness() {
+  ddcutil getvcp 10 | awk '{print $9}' | sed 's/,//'
+}
+
+notify_user() {
+  eval "$notify_cmd 'Brightness: $(get_brightness)%'"
+}
+
+inc_brightness() {
+  ddcutil setvcp 10 + 25 && notify_user
+}
+
+dec_brightness() {
+  ddcutil setvcp 10 - 25 && notify_user
+}
+
+# Execute accordingly
+if [ "$1" = "--get" ]; then
+  get_brightness
+elif [ "$1" = "--inc" ]; then
+  inc_brightness
+elif [ "$1" = "--dec" ]; then
+  dec_brightness
+else
+  echo "$(get_brightness)"% && notify_user
+fi
