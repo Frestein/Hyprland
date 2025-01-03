5d1f77340998ef098c452a3f10e78177fec5677c
Frestein <Frestein@tuta.io>
2025-01-03 17:09:59 +0500

    feat: add bluelight.sh

---
+++ scripts/bluelight.sh
@@ -0,0 +1,17 @@
+#!/bin/dash
+
+notify_cmd='notify-send -u low -h string:x-dunst-stack-tag:bluelight'
+
+notify_user() {
+	eval "$notify_cmd 'Bluelight: $1'"
+}
+
+pid=$(pgrep -x hyprsunset)
+
+if [ "$pid" = "" ]; then
+	hyprsunset &
+	notify_user "Started"
+else
+	kill "$pid"
+	notify_user "Stopped"
+fi
