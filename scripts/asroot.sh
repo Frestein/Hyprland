6812de75ed8d15cfe4e7ffd9e5a7166271b94da2
Frestein <Frestein@tuta.io>
2025-01-04 06:27:27 +0500

    refactor: update

--- scripts/asroot.sh
+++ scripts/asroot.sh
@@ -3,7 +3,7 @@
 dir="$HOME/.config/hypr"
 
 # Export sudo askpass helper
-export SUDO_ASKPASS="$dir/scripts/fuzzel_askpass.sh"
+export SUDO_ASKPASS="$dir/scripts/fuzzel/askpass.sh"
 
 # Execute the application
 sudo -A env XDG_RUNTIME_DIR="$XDG_RUNTIME_DIR" WAYLAND_DISPLAY="$WAYLAND_DISPLAY" XDG_SESSION_TYPE="$XDG_SESSION_TYPE" "$@"
