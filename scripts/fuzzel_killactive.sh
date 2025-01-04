ce19a777beb3f165487d99b51db42fb727f1b444
Frestein <Frestein@tuta.io>
2025-01-04 05:37:01 +0500

    refactor: clean up and improve logic

---
+++ scripts/fuzzel_killactive.sh
@@ -0,0 +1,5 @@
+#!/bin/dash
+
+dir="$HOME/.config/hypr"
+
+[ "$(printf "Yes\nNo" | fuzzel -d -w 16 -l 2 --config="$dir/fuzzel/fuzzel.ini" -p "Close Window? ")" = "Yes" ] && hyprctl dispatch killactive
--- scripts/reload.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/dash
-
-hyprctl reload
-pkill -SIGUSR2 waybar
-notify-send -u low -h string:x-dunst-stack-tag:hypr-reload 'Config reloaded'
-
