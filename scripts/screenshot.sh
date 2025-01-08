e8223a06d827374733ae4e90996890f9b50f4ec0
Frestein <Frestein@tuta.io>
2025-01-09 04:37:10 +0500

    refactor(screenshot): total rewrite, use satty and hyprshot instead of flameshot

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -1,79 +1,61 @@
 #!/bin/dash
 
-# File name
-time=$(date +%Y-%m-%d-%H-%M-%S)
-geometry=$(wlr-randr | grep "current" | awk "{print $1}")
+time=$(date +%Y-%m-%d_%H:%M:%S)
 screenshots_dir="$(xdg-user-dir PICTURES)/Screenshots"
-file="Screenshot_${time}_${geometry}.png"
+file="Screenshot_${time}.png"
 
-# Screenshots directory
 if [ ! -d "$screenshots_dir" ]; then
-  mkdir -p "$screenshots_dir"
+	mkdir -p "$screenshots_dir"
 fi
 
-# Notify and view screenshot
-notify_view() {
-  notify_cmd_shot='notify-send -u low -h string:x-dunst-stack-tag:flameshot -i /usr/share/archcraft/icons/dunst/picture.png'
-  eval "$notify_cmd_shot 'Copied to clipboard.'"
-  paplay /usr/share/sounds/freedesktop/stereo/screen-capture.oga >/dev/null 2>&1 &
-  if [ -e "$screenshots_dir/$file" ] && [ -s "$screenshots_dir/$file" ]; then
-    eval "$notify_cmd_shot 'Screenshot saved.'"
-    qview "${screenshots_dir}/$file"
-  else
-    eval "$notify_cmd_shot 'Screenshot aborted.'"
-  fi
-}
-
-copy_shot() {
-  tee "$screenshots_dir/$file" | wl-copy --type image/png
-}
-
 countdown() {
-  seq "$1" -1 1 | while read -r sec; do
-    notify-send -t 1000 -h string:x-dunst-stack-tag:screenshottimer -i /usr/share/archcraft/icons/dunst/timer.png "Taking shot in: $sec"
-    sleep 1
-  done
+	seq "$1" -1 1 | while read -r sec; do
+		notify-send -t 1000 -h string:x-dunst-stack-tag:screenshot "Taking shot in: $sec"
+		sleep 1
+	done
 }
 
 shot() {
-  mode="$1"
-  shift
-  extra_args="$*"
-  eval flameshot "$mode" "$extra_args" -r | copy_shot
-  notify_view
+	mode="$1"
+	shift
+	extra_args="$*"
+	hyprshot -m "$mode" --raw --freeze "$extra_args" | satty --filename - --output-filename "$screenshots_dir/$file"
 }
 
 shotnow() {
-  shot screen
+	shot output -m HDMI-A-2
 }
 
-shot5() {
-  countdown 5
-  sleep 0.5
-  shot screen
+shotdelayed() {
+	countdown "$1"
+	sleep 0.5
+	shot output -m HDMI-A-2
 }
 
-shot10() {
-  countdown 10
-  sleep 0.5
-  shot screen
+shotwin() {
+	shot window
+}
+
+shotactive() {
+	shot window -m active
 }
 
 shotarea() {
-  shot gui
+	shot region
 }
 
-# Execute
 if [ "$1" = "--now" ]; then
-  shotnow
-elif [ "$1" = "--in5" ]; then
-  shot5
-elif [ "$1" = "--in10" ]; then
-  shot10
+	shotnow
+elif [ "$1" = "--delay" ] && [ "$2" -ge 1 ] && [ "$2" -le 9 ]; then
+	shotdelayed "$2"
+elif [ "$1" = "--win" ]; then
+	shotwin
+elif [ "$1" = "--active" ]; then
+	shotactive
 elif [ "$1" = "--area" ]; then
-  shotarea
+	shotarea
 else
-  echo "Available options: --now --in5 --in10 --area"
+	echo "Available options: --now --delay [1-9] --win --active --area"
 fi
 
 exit 0
