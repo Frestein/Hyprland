87af15a555792c48f3f04410badfe53827e8b09d
Frestein <frestein@tuta.io>
2025-03-06 01:23:59 +0500

    feat: update

--- scripts/killactive.sh
+++ scripts/killactive.sh
@@ -4,13 +4,13 @@ LOCK_FILE="/tmp/killactive.lock"
 TIMEOUT=300
 
 if [ -f "$LOCK_FILE" ]; then
-	rm -f "$LOCK_FILE"
-	hyprctl dispatch killactive
-	exit 0
+    rm -f "$LOCK_FILE"
+    hyprctl dispatch killactive
+    exit 0
 fi
 
 touch "$LOCK_FILE"
 (
-	sleep "$(echo "$TIMEOUT / 1000" | bc -l)"
-	rm -f "$LOCK_FILE"
+    sleep "$(echo "$TIMEOUT / 1000" | bc -l)"
+    rm -f "$LOCK_FILE"
 ) &
